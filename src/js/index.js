93fe5fe5b16fc77024d8d6ac979018114d14c5de
no15_renne <no15_renne@yahoo.co.jp>
2019-01-01 22:37:11 +0900

    jqueryを利用しないように修正

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import multiTagSearch from './components/multiTagSearch.js'
 
 setTimeout(() => {
-  const tagContainer = $('.TagContainer');
-  tagContainer.after(`<div id="multi-tag-search-app"><multi-tag-search></multi-tag-search></div>`);
+  const tagContainer = document.querySelectorAll('.TagContainer')[0];
+  tagContainer.insertAdjacentHTML("afterend",'<div id="multi-tag-search-app"><multi-tag-search></multi-tag-search></div>');
   new Vue({ el: '#multi-tag-search-app' });
 }, 1000);
